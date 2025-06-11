d03d2857a8dad18b0e3e8ce0ba014e25b8d81124
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2025-06-11 14:53:02 -0300

    feat: add logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,13 @@
+import { Link } from 'react-router-dom'
+import Logo from '../../assets/dnc-logo.png'
 export default function Header(){
+
+    // ASSETS
     return (
         <header>
             <div className="container">
                 <div className="d-flex al-center jc-space-between">
-
+                    <Link to="/"><img src={Logo} /></Link>
                 </div>
             </div>
         </header>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,9 @@
+import Header from "../components/Header/Header";
+
 export default function Home() {
      return (
         <>
+        <Header />
         <h1>Home</h1>
         <p>Subtitle</p>
         </>
