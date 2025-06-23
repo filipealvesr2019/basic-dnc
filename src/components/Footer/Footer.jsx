3b4459092b0f5e7e0d96d1f714b78c4e6bf24b7c
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2025-06-23 15:55:13 -0300

    feat: add Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,31 +1,44 @@
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
-import Facebook from '../../assets/facebookLogo.png';
-import Intagram from '../../assets/facebookLogo.png';
-import Linkedin from '../../assets/facebookLogo.png';
-import Twiter from '../../assets/facebookLogo.png';
+import Facebook from "../../assets/facebookLogo.png";
+import Intagram from "../../assets/facebookLogo.png";
+import Linkedin from "../../assets/facebookLogo.png";
+import Twiter from "../../assets/facebookLogo.png";
 
-import BrasilLogo from '../../assets/facebookLogo.png';
-import USALogo from '../../assets/facebookLogo.png';
-import Logo from '../../assets/dnc-logo.png'
+import BrasilLogo from "../../assets/facebookLogo.png";
+import USALogo from "../../assets/facebookLogo.png";
+import Logo from "../../assets/dnc-logo.png";
 
-import './Footer.css'
-export default function Footer(){
-
-    // ASSETS
-    return (
-        <footer>
-            <div className="container">
-                <div className="d-flex jc-space-between">
-                    <div className='footer-logo-col'>
-                 <img src={Logo} alt="" />
-                 <p className='gray-1-color'>A escola que prepara você para as profissões em alta no mercado de trabalho.</p>
-
-                    </div>
-           
-                </div>
-              
+import "./Footer.css";
+export default function Footer() {
+  // ASSETS
+  return (
+    <footer>
+      <div className="container">
+        <div className="d-flex jc-space-between">
+          <div className="footer-logo-col">
+            <img src={Logo} alt="" />
+            <p className="gray-1-color">
+              A escola que prepara você para as profissões em alta no mercado de
+              trabalho.
+            </p>
+            <div className="d-flex social-links">]
+                <a href="https://google.com" target="_blank">
+                    <img src={Facebook}  />
+                </a>
+                 <a href="https://google.com" target="_blank">
+                    <img src={Twiter}  />
+                </a>
+                  <a href="https://google.com" target="_blank">
+                    <img src={Linkedin}  />
+                </a>
+                  <a href="https://google.com" target="_blank">
+                    <img src={Intagram}  />
+                </a>
             </div>
-        </footer>
-    )
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
