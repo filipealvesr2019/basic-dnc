78437d8696a4f880b2d626e6181c220eac639c39
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2025-07-11 16:11:34 -0300

    feat: add componente de butão.

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,8 +1,8 @@
 import './Button.css'
 export default function Button({ arrow, buttonStyle, loading, children, ...props}){
     return (
-        <>
-        
-        </>
+        <button className={`button ${buttonStyle}`}>
+            {children}
+        </button>
     )
 }
