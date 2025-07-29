82e4ed29fb6438142266ffb9677da87a33c24ad9
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2025-07-29 15:22:29 -0300

    feat: adiciona componente de banner.

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -2,8 +2,12 @@ import './Banner.css'
 
 export default function Banner(props) {
      return (
-        <div className='banner d-flex al-end' style={{ }}>
-
+        <div className='banner d-flex al-end' style={{ backgroundImage: `url(/${props.image})`}}>
+         <div className='container'>
+            <div className='title-panel d-flex al-center jc-center'>
+               <h1>{props.title}</h1>
+            </div>
+         </div>
         </div>
      )
 }
