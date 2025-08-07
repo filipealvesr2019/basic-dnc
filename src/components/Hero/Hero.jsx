2a69516b818ce6dd1d3dbaaf982f71e77c2d6382
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2025-08-07 14:47:55 -0300

    feat: adiciona componente de hero.

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,11 +1,18 @@
+import { Link } from 'react-router-dom'
 import './Hero.css'
 
-export default function Hero(props) {
+export default function Hero() {
      return (
         <div className='hero d-flex al-center' style={{ backgroundImage: `url(/${props.image})`}}>
          <div className='hero-text'>
             <h1>Let Your Home Be Unique</h1>
             <p>There are many variations of the passages of lorem Ipsum fromavailable, majority.</p>
+            <Link href={"/"}>
+            <Button buttonStyle="secondary" arrow>
+               Get Started
+            </Button>
+
+            </Link>
          </div>
         </div>
      )
