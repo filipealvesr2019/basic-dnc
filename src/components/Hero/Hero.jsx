ae453dc0abcb3f8c106777222c97cdbf92bb1fae
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2025-08-08 15:21:49 -0300

    feat: adiciona componente de hero.

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,9 +1,10 @@
 import { Link } from 'react-router-dom'
 import './Hero.css'
+import Button from '../Button/Button'
 
 export default function Hero() {
      return (
-        <div className='hero d-flex al-center' style={{ backgroundImage: `url(/${props.image})`}}>
+        <div className='hero d-flex al-center'>
          <div className='hero-text'>
             <h1>Let Your Home Be Unique</h1>
             <p>There are many variations of the passages of lorem Ipsum fromavailable, majority.</p>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
-import Banner from "../components/Banner/Banner";
 import Button from "../components/Button/Button";
 import Footer from "../components/Footer/Footer";
 import Header from "../components/Header/Header";
+import Hero from "../components/Hero/Hero";
 
 export default function Home() {
      return (
@@ -9,8 +9,8 @@ export default function Home() {
         <Header />
         <h1>Home</h1>
         <p>Subtitle</p>
-        <Button buttonStyle="primary" arrow>Ola!</Button>
-        <Banner title="Home" image="about.png"/>
+        <Button buttonStyle="secondary" arrow>Ola!</Button>
+        <Hero />
         <Footer />
         </>
      )
