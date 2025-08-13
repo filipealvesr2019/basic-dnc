60b16ecc7e1b6e9d99874cbb7405d68a6f78734c
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2025-08-13 15:26:20 -0300

    feat: adiciona componente de hero.

---
+++ src/components/ProjectsList/ProjectsList.jsx
@@ -0,0 +1,12 @@
+import './ProjectsList.css'
+
+export default function ProjectsList(props) {
+     return (
+        <div className='projects-section'>
+         <div className='projects-hero'>
+            <h2>Follow Our Projects</h2>
+            <p>It is a long established fact that a reader will be distracted by the of readable content of page  lookings at its layouts  points.</p>
+         </div>
+        </div>
+     )
+}
