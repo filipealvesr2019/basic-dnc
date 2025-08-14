cc5d611b22874db1e8c90108a3ee55d8bbd9fcc7
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2025-08-14 15:15:03 -0300

    feat: adiciona componente de hero.

--- src/components/ProjectsList/ProjectsList.jsx
+++ src/components/ProjectsList/ProjectsList.jsx
@@ -7,6 +7,16 @@ export default function ProjectsList(props) {
             <h2>Follow Our Projects</h2>
             <p>It is a long established fact that a reader will be distracted by the of readable content of page  lookings at its layouts  points.</p>
          </div>
+         <div className='projects-grid'>
+            <div className='project-card d-flex jc-center fd-column'>
+               <div className='thumb tertiary-background'>
+
+               </div>
+               <h3>Pedro Alves</h3>
+               <p>São Paulo, Brasil</p>
+               <img src={} alt="" />
+            </div>
+         </div>
         </div>
      )
 }
