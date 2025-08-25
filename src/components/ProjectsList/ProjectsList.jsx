13a9e9b1ace58025b1bfede42c133f3e13d3f171
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2025-08-25 14:21:39 -0300

    feat: update ProjectsList styles for improved layout and card presentation

--- src/components/ProjectsList/ProjectsList.jsx
+++ src/components/ProjectsList/ProjectsList.jsx
@@ -1,79 +1,66 @@
-import './ProjectsList.css';
-import LikedFilled from '../../assets/like_filled.svg'
+import "./ProjectsList.css";
+import LikedFilled from "../../assets/like_filled.svg";
 
 export default function ProjectsList(props) {
-     return (
-        <div className='projects-section'>
-         <div className='projects-hero'>
-            <h2>Follow Our Projects</h2>
-            <p>It is a long established fact that a reader will be distracted by the of readable content of page  lookings at its layouts  points.</p>
-         </div>
-         <div className='projects-grid'>
-            <div className='project-card d-flex jc-center fd-column'>
-               <div className='thumb tertiary-background'>
-
-               </div>
-               <h3>Pedro Alves</h3>
-               <p>São Paulo, Brasil</p>
-               <img src={LikedFilled} alt="" className='img'/>
-            </div>
-              <div className='project-card d-flex jc-center fd-column'>
-               <div className='thumb tertiary-background'>
-
-               </div>
-               <h3>Pedro Alves</h3>
-               <p>São Paulo, Brasil</p>
-               <img src={LikedFilled} alt="" className='img'/>
-            </div>
-              <div className='project-card d-flex jc-center fd-column'>
-               <div className='thumb tertiary-background'>
-
-               </div>
-               <h3>Pedro Alves</h3>
-               <p>São Paulo, Brasil</p>
-               <img src={LikedFilled} alt="" className='img'/>
-            </div>
-              <div className='project-card d-flex jc-center fd-column'>
-               <div className='thumb tertiary-background'>
-
-               </div>
-               <h3>Pedro Alves</h3>
-               <p>São Paulo, Brasil</p>
-               <img src={LikedFilled} alt="" className='img'/>
-            </div>
-              <div className='project-card d-flex jc-center fd-column'>
-               <div className='thumb tertiary-background'>
-
-               </div>
-               <h3>Pedro Alves</h3>
-               <p>São Paulo, Brasil</p>
-               <img src={LikedFilled} alt="" className='img'/>
-            </div>
-              <div className='project-card d-flex jc-center fd-column'>
-               <div className='thumb tertiary-background'>
-
-               </div>
-               <h3>Pedro Alves</h3>
-               <p>São Paulo, Brasil</p>
-               <img src={LikedFilled} alt="" className='img'/>
-            </div>
-              <div className='project-card d-flex jc-center fd-column'>
-               <div className='thumb tertiary-background'>
-
-               </div>
-               <h3>Pedro Alves</h3>
-               <p>São Paulo, Brasil</p>
-               <img src={LikedFilled} alt="" className='img'/>
-            </div>
-              <div className='project-card d-flex jc-center fd-column'>
-               <div className='thumb tertiary-background'>
-
-               </div>
-               <h3>Pedro Alves</h3>
-               <p>São Paulo, Brasil</p>
-               <img src={LikedFilled} alt="" className='img'/>
-            </div>
-         </div>
+  return (
+    <div className="projects-section">
+      <div className="projects-hero">
+        <h2>Follow Our Projects</h2>
+        <p>
+          It is a long established fact that a reader will be distracted by the
+          of readable content of page lookings at its layouts points.
+        </p>
+      </div>
+      <div className="projects-grid">
+        <div className="project-card d-flex jc-center fd-column">
+          <div className="thumb tertiary-background"></div>
+          <h3>Pedro Alves</h3>
+          <p>São Paulo, Brasil</p>
+          <img src={LikedFilled} alt="" className="img" />
         </div>
-     )
+        <div className="project-card d-flex jc-center fd-column">
+          <div className="thumb tertiary-background"></div>
+          <h3>Pedro Alves</h3>
+          <p>São Paulo, Brasil</p>
+          <img src={LikedFilled} alt="" className="img" />
+        </div>
+        <div className="project-card d-flex jc-center fd-column">
+          <div className="thumb tertiary-background"></div>
+          <h3>Pedro Alves</h3>
+          <p>São Paulo, Brasil</p>
+          <img src={LikedFilled} alt="" className="img" />
+        </div>
+        <div className="project-card d-flex jc-center fd-column">
+          <div className="thumb tertiary-background"></div>
+          <h3>Pedro Alves</h3>
+          <p>São Paulo, Brasil</p>
+          <img src={LikedFilled} alt="" className="img" />
+        </div>
+        <div className="project-card d-flex jc-center fd-column">
+          <div className="thumb tertiary-background"></div>
+          <h3>Pedro Alves</h3>
+          <p>São Paulo, Brasil</p>
+          <img src={LikedFilled} alt="" className="img" />
+        </div>
+        <div className="project-card d-flex jc-center fd-column">
+          <div className="thumb tertiary-background"></div>
+          <h3>Pedro Alves</h3>
+          <p>São Paulo, Brasil</p>
+          <img src={LikedFilled} alt="" className="img" />
+        </div>
+        <div className="project-card d-flex jc-center fd-column">
+          <div className="thumb tertiary-background"></div>
+          <h3>Pedro Alves</h3>
+          <p>São Paulo, Brasil</p>
+          <img src={LikedFilled} alt="" className="img" />
+        </div>
+        <div className="project-card d-flex jc-center fd-column">
+          <div className="thumb tertiary-background"></div>
+          <h3>Pedro Alves</h3>
+          <p>São Paulo, Brasil</p>
+          <img src={LikedFilled} alt="" className="img" />
+        </div>
+      </div>
+    </div>
+  );
 }
