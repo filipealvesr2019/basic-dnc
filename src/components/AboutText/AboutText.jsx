f82e125efb9f07a5a883222e1637205e3f27d82e
opaulorom <paulinhop2017@gmail.com>
2025-09-08 15:17:28 -0300

    feat: Inserindo componentes na página de contato

---
+++ src/components/AboutText/AboutText.jsx
@@ -0,0 +1,16 @@
+import './AboutText.css'
+
+export default function AboutText(props) {
+     return (
+         <div className='container'>
+            <div className='text-section d-flex'>
+               <h2>ABC Ser</h2>
+            </div>
+            <div className='text-section d-flex'>
+               <p className='primary-color'>Primeira linha</p>
+               <p className='primary-color'>Segunda linha</p>
+               <p className='primary-color'>Terceira linha</p>
+            </div>
+         </div>
+     )
+}
