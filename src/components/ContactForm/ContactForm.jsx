288abf271eb93be6dec494ef584631c2f7847b41
opaulorom <filipealvesrds2019@gmail.com>
2025-09-18 15:10:12 -0300

    add

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,11 +1,11 @@
 export default function ContactForm(){
     return (
           <div className='container'>
-            <div className="contact-form d-flex fd-column">
+            <div className="contact-form d-flex fd-column al-center">
                 
             </div>
             <h2>We love meeting new people and helping them.</h2>
-        <form action=""></form>
+        <form action="">Wip..</form>
          </div>
     )
 }
