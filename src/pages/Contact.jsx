6d8602e3b591ba9afd5a0df815c7df38545d1064
opaulorom <filipealvesrds2019@gmail.com>
2025-09-19 15:41:30 -0300

    add

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,18 @@
+import Banner from '../components/Banner/Banner'
+import ContactForm from '../components/ContactForm/ContactForm'
+import Footer from '../components/Footer/Footer'
+import Header from '../components/Header/Header'
 export default function Contact(){
     return (
           <>
-        <h1>Contact</h1>
-        <p>Subtitle</p>
+        <Header />
+                     <h1>Home</h1>
+                     <p>Subtitle</p>
+                     <Banner title="Contact" image="contact.png" />
+                     <div className="container">
+                     <ContactForm />
+                     </div>
+                     <Footer />
         </>
     )
 }
