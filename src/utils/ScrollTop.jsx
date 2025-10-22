7b6beb96d45702d0c723f2ea448d12522c2ff893
opaulorom <paulinhop2017@gmail.com>
2025-10-22 15:09:55 -0300

    add

--- src/utils/ScrollTop.jsx
+++ src/utils/ScrollTop.jsx
@@ -1,6 +1,10 @@
 import { useEffect } from 'react';
-import { useLocation } from 'react';
+import { useLocation } from 'react-router-dom';
 
 const ScrollTop = () => {
+    const { pathname } = useLocation;
 
+    useEffect(() => {
+        window.scrollTo(0, 0)
+    })
 }
