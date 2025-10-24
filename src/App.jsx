0e26ee1478d6b7a5504132bbcbab40d7abde37fe
opaulorom <paulinhop2017@gmail.com>
2025-10-24 14:50:29 -0300

    add

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Projects from "./pages/Projects";
 import Contact from './pages/Contact';
+import ScrollToTop from "./utils/ScrollToTop";
 
 // UTILS
 
