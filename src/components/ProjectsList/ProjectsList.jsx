7a3e5372336ccaa0ba22c95566fa73396922f9fe
filipealvesr2019 <filipealvesrds2019@gmail.com>
2025-12-16 15:35:32 -0300

    add

--- src/components/ProjectsList/ProjectsList.jsx
+++ src/components/ProjectsList/ProjectsList.jsx
@@ -14,11 +14,11 @@ const thumbsMock = [
   "https://images.unsplash.com/photo-1586023492125-27b2c045efd7?q=80&w=658&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   "https://images.unsplash.com/photo-1657907157592-dd6cfb9d06cf?q=80&w=800&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   "https://images.unsplash.com/photo-1523217582562-09d0def993a6?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fGhvdXNlfGVufDB8fDB8fHww",
-  "https://source.unsplash.com/random/400x300?building",
-  "https://source.unsplash.com/random/400x300?work",
-  "https://source.unsplash.com/random/400x300?studio",
-  "https://source.unsplash.com/random/400x300?workspace",
-  "https://source.unsplash.com/random/400x300?decoration",
+  "https://images.unsplash.com/photo-1585128792020-803d29415281?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjN8fHJvb218ZW58MHx8MHx8fDA%3D",
+  "https://images.unsplash.com/photo-1630699144919-681cf308ae82?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzR8fHJvb218ZW58MHx8MHx8fDA%3D",
+  "https://images.unsplash.com/photo-1513161455079-7dc1de15ef3e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDB8fHJvb218ZW58MHx8MHx8fDA%3D",
+  "https://images.unsplash.com/photo-1484101403633-562f891dc89a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDZ8fHJvb218ZW58MHx8MHx8fDA%3D",
+  "https://images.unsplash.com/photo-1560448204-e02f11c3d0e2?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDd8fHJvb218ZW58MHx8MHx8fDA%3D",
 ];
 
 
