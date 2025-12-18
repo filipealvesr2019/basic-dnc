ec20f2773e62a465c5532df09f69d0fd05e11ec6
filipealvesr2019 <filipealvesrds2019@gmail.com>
2025-12-18 16:37:40 -0300

    add

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,19 +1,33 @@
-export default function ContactForm(){
-    return (
-          <div className='container'>
-            <div className="contact-form d-flex fd-column al-center">
-                
-            </div>
-            <h2>We love meeting new people and helping them.</h2>
-        <form action="">
-            <div className="d-flex form-group">
-                <input 
-                className="form-input"
-                type="text"
-                id="name"
-                name="name"
-            </div>
-        </form>
-         </div>
-    )
+export default function ContactForm() {
+  return (
+    <div className="container">
+      <div className="contact-form d-flex fd-column al-center"></div>
+      <h2>We love meeting new people and helping them.</h2>
+      <form action="">
+        <div className="d-flex form-group">
+          <input
+            className="form-input"
+            type="text"
+            id="name"
+            name="name"
+            placeholder="Name *"
+          />
+          <input
+            className="form-input"
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Email *"
+          />
+          <textarea 
+            className="form-input"
+            id="message"
+            name="message"
+            placeholder="Mensagem *"
+            rows="4"
+            />
+        </div>
+      </form>
+    </div>
+  );
 }
